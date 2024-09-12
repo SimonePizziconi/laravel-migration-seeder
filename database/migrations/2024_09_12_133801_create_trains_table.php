14cd7f369649fa04138a6bd9010855f6e6f8dd4e
SimonePizziconi <simonepizziconi05@gmail.com>
2024-09-12 15:52:54 +0200

    feat: migrate trains table

--- database/migrations/2024_09_12_133801_create_trains_table.php
+++ database/migrations/2024_09_12_133801_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('agency', 50);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 20);
+            $table->integer('number_of_carriages');
+            $table->boolean('on_time')->default(true);
+            $table->boolean('cancelled')->default(false);
             $table->timestamps();
         });
     }
