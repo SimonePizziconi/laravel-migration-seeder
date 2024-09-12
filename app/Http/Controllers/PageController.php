d8b74c37b1774093e7fec2911fb68dbed12fee24
SimonePizziconi <simonepizziconi05@gmail.com>
2024-09-12 16:42:01 +0200

    feat:Print on page all trains departing today

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,11 +6,18 @@ use Illuminate\Http\Request;
 
 use App\Models\Train;
 
+use Illuminate\Support\Carbon;
+
 class PageController extends Controller
 {
     public function index()
     {
-        $trains = Train::all();
+        // Ottieni la data odierna
+        $today = Carbon::today();
+
+        // Recupera tutti i treni in partenza dalla data odierna
+        $trains = Train::whereDate('departure_date', '=', $today)->get();
+
         return view('home', compact('trains'));
     }
     public function about()
